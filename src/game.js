44447f19467d6b7e37f2ce183669467a85e7c889
Ricky Keisling <rkeisling@basecampcodingacademy.org>
2016-10-13 13:19:51 -0500

    Added all vital files for testing

---
+++ src/game.js
@@ -0,0 +1,8 @@
+Game = {
+  // Initialize and start the Game
+  start: function() {
+    // Start crafty and set bg color to see if its working
+    Crafty.init(480, 320);
+    Crafty.background('green');
+  }
+}
