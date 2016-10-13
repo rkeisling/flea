caf1ab6c3bb5046e615445294473e68ffbb6d97d
Ricky Keisling <rkeisling@basecampcodingacademy.org>
2016-10-13 14:15:32 -0500

    Beginning to figure out crafty

--- src/game.js
+++ src/game.js
@@ -1,8 +1,20 @@
-Game = {
-  // Initialize and start the Game
-  start: function() {
-    // Start crafty and set bg color to see if its working
-    Crafty.init(480, 320);
-    Crafty.background('green');
-  }
-}
+Crafty.init(500,350, document.getElementById('game'));
+// Crafty.e('2D, DOM, Color').attr({x: 0, y: 0, w: 100, h: 100}).color('#F00');
+var floor = Crafty.e('Floor, 2D, Canvas, Color');
+floor.attr({
+  x: 0,
+  y: 250,
+  w: 500,
+  h: 10
+}).color('green');
+var block = Crafty.e('2D, Canvas, Color, Twoway, Gravity');
+block.attr({
+  x: 0,
+  y: 0,
+  w: 50,
+  h: 50
+}).color('#F00').twoway(200).gravity('Floor');
+block.bind('EnterFrame', function(){
+  this.rotation = this.rotation + 3;
+});
+block.origin('center');
