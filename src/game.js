24103f70e6d445d265e5ba20100cb78264901ae5
Dustin Buice <dbuice@basecampcodingacademy.org>
2016-10-14 15:25:06 -0500

    changed time to 10 minuntes

--- src/game.js
+++ src/game.js
@@ -44,18 +44,22 @@ flea.attr({
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 10);
   return {
+    'minutes': minutes,
     'seconds': seconds
   };
 }
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
+  var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.seconds <= 0) {
