4aecc7cc433e09655b1dfb8ef9c1b0aa4f220904
Dustin Buice <dbuice@basecampcodingacademy.org>
2016-10-18 14:50:12 -0500

    fixed error now time is set to 3

--- src/game.js
+++ src/game.js
@@ -44,7 +44,7 @@ flea.attr({
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 4);
+  var minutes = Math.floor((t / 1000 / 60) % 3);
   return {
     'minutes': minutes,
     'seconds': seconds
